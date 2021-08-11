ee3b3b6fcba1ebc102712336c39d9805ed3e3ebb
Denis070 <d7dev@bk.ru>
2021-08-11 12:33:00 +0300

    Публикация видео https://youtu.be/WQqxA8R8YaQ

--- examples/e6_1_charts.py
+++ examples/e6_1_charts.py
@@ -4,32 +4,22 @@ from openpyxl.chart import Reference, BarChart, LineChart
 
 def example(filename="test.xlsx"):
     """
+    Добавление графиков в Excel с пакетом OpenPyXl
     https://openpyxl.readthedocs.io/en/stable/charts/introduction.html
+    Этот код для видео https://youtu.be/WQqxA8R8YaQ
     :return:
     """
     book = openpyxl.load_workbook(filename)
     sheet = book.active
 
     chart = LineChart()
-    # chart = BarChart()
 
-    # chart.add_data("Коллеги!C2:C99")
+    chart.anchor="J5"
+    chart.width=15 # in cm
+    chart.height=5 # in cm
 
-    # data = Reference(sheet, min_col=3, max_col=4, min_row=2, max_row=99)
-    # print(data)
-    # data = Reference(sheet, range_string="Коллеги!C2:C99")
-    # print([data, data.min_col, data.max_col, data.min_row, data.max_row])
-    # chart.add_data(data)
+    data = Reference(sheet, min_col=3, max_col=4, min_row=2, max_row=99)
+    chart.add_data(data)
 
-    # chart.anchor="J5"
-    # chart.width=15 # in cm
-    # chart.height=5 # in cm
-
-    # By default the top-left corner of a chart is anchored to cell E15 and the size is 15 x 7.5 cm (approximately 5 columns by 14 rows)
-    # активной таблицы
     sheet.add_chart(chart)
-
-    # см. class ChartBase
-    # sheet.add_chart(chart, "D10")
-
     book.save(filename)
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Python + openpyxl = Excel
 
-Этот код для [видео](https://youtu.be/dn3Oi7oaMT4) на канале [Azzrael Code](https://www.youtube.com/channel/UCf6kozNejHoQuFhBDB8cfxA) где я разбираюсь как с помощью Питона и бибилиотеки 
+Этот код для [Плейлиста об OpenPyXl](https://www.youtube.com/watch?v=dn3Oi7oaMT4&list=PLWVnIRD69wY51SUWhDp8lwA-1kJorY4Za) на канале [Azzrael Code](https://www.youtube.com/channel/UCf6kozNejHoQuFhBDB8cfxA) где я разбираюсь как с помощью Питона и бибилиотеки 
 [openpyxl](https://openpyxl.readthedocs.io/en/stable/) создавать и изменять экселевские таблицы.
 
 Для установки
